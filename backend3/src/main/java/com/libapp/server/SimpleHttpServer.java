9cfaa73cf3660be6cae79baae3a93ca7c43c4a14
dkowalczyk <d.kowalczyk@dituel.pl>
2024-10-30 10:09:33 +0100

    fix db connection, restructure project ot use maven

---
+++ backend3/src/main/java/com/libapp/server/SimpleHttpServer.java
@@ -0,0 +1,25 @@
+package com.libapp.server;
+
+import com.sun.net.httpserver.HttpServer;
+import com.libapp.handler.AddBookHandler;
+import com.libapp.handler.GetBookHandler;
+import com.libapp.handler.PopularBooksHandler;
+import com.libapp.handler.SearchBooksHandler;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class SimpleHttpServer {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+        server.createContext("/api/books/add", new AddBookHandler());
+        server.createContext("/api/books/search", new SearchBooksHandler());
+        server.createContext("/api/books", new GetBookHandler());
+        server.createContext("/api/books/popular", new PopularBooksHandler()); // New endpoint for popular books
+
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println("Server started on port 8081");
+    }
+}
+
